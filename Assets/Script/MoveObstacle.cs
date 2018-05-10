a862c30891f446cd9168ae941c1f448fc2a3ae70
CHP\aramkim <locomotor.control@gmail.com>
2018-05-10 11:06:00 -0700

    Time

--- Assets/Script/MoveObstacle.cs
+++ Assets/Script/MoveObstacle.cs
@@ -12,9 +12,10 @@ public class MoveObstacle : MonoBehaviour {
 	private float correctpos;
     private float speed = 0;
     private float treadmillspeed;
+    private bool hasStarted = false;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start () 
 	{
 		obdynamicpred = PlayerPrefs.GetInt("ObDynamicPred");
         treadmillspeed = PlayerPrefs.GetFloat("TreadmillInput") / 2.23694f;
@@ -31,16 +32,18 @@ public class MoveObstacle : MonoBehaviour {
 
 		else if (obdynamicpred == 3) 
 		{
-            speed = treadmillspeed * 1.5f;
+            speed = treadmillspeed * 1.1f;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{		
-		startpos_continue = new Vector3(startpos.x - parentpos.x, startpos.y - parentpos.y, startpos.z - parentpos.z);
-		endpos_continue = new Vector3 (startpos_continue.x + 0.5f, startpos_continue.y, startpos_continue.z);		
+		//startpos_continue = new Vector3(startpos.x - parentpos.x, startpos.y - parentpos.y, startpos.z - parentpos.z);
+		//endpos_continue = new Vector3 (startpos_continue.x + 0.5f, startpos_continue.y, startpos_continue.z);		
         transform.Translate(Vector2.right * Time.deltaTime * speed);
+        Debug.Log(Time.deltaTime);
+        
     }
 }
 
